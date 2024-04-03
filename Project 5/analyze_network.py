1498ff00d7b7e646f060a12132550d99e095aed6
sam <lee.samuel5@northeastern.edu>
2024-04-02 20:51:45 -0400

    finished implementing conv1 layer tasks)

---
+++ Project 5/analyze_network.py
@@ -0,0 +1,135 @@
+# Samuel Lee
+# CS 5330
+# Spring 2024
+# This program...
+
+# import statements
+import cv2
+import torch
+import load_tutorial
+import train_tutorial
+import sys
+import matplotlib.pyplot as plt
+from torchvision.transforms import ToTensor, Compose, Normalize
+
+
+# this function prints the model and first layer and visualizes the first layer via PyPlot
+def printAndVisualizeFirstLayer(model):
+    # shows network model and layers used
+    print("Network Model")
+    print("-----------------------------------------------------")
+    print(model)
+    # first layer
+    print("First Layer")
+    print("-----------------------------------------------------")
+    weights = model.conv1.weight.detach().cpu().numpy()
+    # set up figure and axes for subplots to visualize the weights
+    figure, axes = plt.subplots(3, 4, figsize=(10, 8))
+    axes = axes.flatten()  # used for linear iteration
+
+    # store filters in array
+    firstLayerFilters = []
+
+    # iterate over each filter
+    for i in range(len(weights)):
+        axis = axes[i]
+        # plot filter with color map 'viridis'
+        axis.imshow(weights[i, 0], cmap="viridis", interpolation="none")
+
+        # format subplot
+        axis.set_title(f"Filter {i}")
+        axis.axis("off")
+
+        # print filter index, shape, and weights
+        print("Filter index:", i)
+        print("Shape:", weights[i, 0].shape)
+        print(weights[i, 0])
+        firstLayerFilters.append(weights[i, 0])
+
+    # hide any unused subplots
+    for i in range(len(weights), len(axes)):
+        axes[i].axis("off")
+
+    # show visualization
+    plt.tight_layout()
+    plt.show()
+
+    return firstLayerFilters
+
+
+# this function returns the first image of a dataloader
+def getFirstImage(dataloader):
+    images, labels = next(iter(dataloader))
+    image = images[0].squeeze().cpu().detach().numpy()
+    return image
+
+
+# this function applies each of the first layer filters to an image
+def applyFirstLayerIndividually(layer, image):
+    # get each filter
+    filters = layer.weight.cpu().detach().numpy()
+
+    # store output images from applying filter
+    results = []
+
+    # apply each filter to the image and store the resulting image
+    with torch.no_grad():
+        for i in range(len(filters)):
+            filter = filters[i, 0]
+            result = cv2.filter2D(image, -1, filter)
+            results.append(result)
+
+    # plot filter and corresponding image
+    figure, axes = plt.subplots(5, 4, figsize=(10, 8))
+    axes = axes.flat
+
+    # loop through each result and filter and plot them
+    for i in range(len(results)):
+        axis = axes[i]
+
+        # plot filter for even axes
+        filter_axis = axes[i * 2]
+        filter_axis.imshow(filters[i, 0], cmap="gray")
+        filter_axis.axis("off")
+
+        # plot resulting image for odd axes
+        result_axis = axes[i * 2 + 1]  # odd index for results
+        result_axis.imshow(results[i], cmap="gray")
+        result_axis.axis("off")
+
+    # show figure
+    plt.tight_layout()
+    plt.show()
+
+
+# main function
+def main(argv):
+    # load network file
+    model, device = load_tutorial.load_network_file("model_weights.pth")
+
+    # print model and layers
+    firstLayerFilters = printAndVisualizeFirstLayer(model)
+
+    # load dataset
+    transform = Compose(
+        [
+            ToTensor(),
+            Normalize(
+                (0.1307,),
+                (0.3081,),
+            ),
+        ]
+    )
+    training_data = train_tutorial.load_data("data", True, True, transform, 64)
+
+    # get first image
+    firstImage = getFirstImage(training_data)
+
+    # apply first layer filter to image based on number of filters and plot the result
+    applyFirstLayerIndividually(model.conv1, firstImage)
+
+    return
+
+
+if __name__ == "__main__":
+    main(sys.argv)
--- Project 5/load_tutorial.py
+++ Project 5/load_tutorial.py
@@ -5,55 +5,143 @@
 
 # import statements
 import train_tutorial
+import cv2
+import os
 import sys
 import torch
-from torch import nn
-from torch.utils.data import Dataset
-from torchvision import datasets
-from torch.utils.data import DataLoader
 from torchvision.transforms import ToTensor, Compose, Normalize
-import torch.nn.functional as F
-import torch.optim as optim
 import matplotlib.pyplot as plt
 
 
-# this function saves the network to a file
-def load_network_file(model, file_path):
-    model_state_dict = torch.load(file_path)
-    model.load_state_dict(model_state_dict)
-
-
-# main function
-def main(argv):
-    # load network file
+# this function loads the network file and stores it in the model
+def load_network_file(file_path):
     device = train_tutorial.set_device()
     model = train_tutorial.NeuralNetwork().to(device)
     model.device = device
-    load_network_file(model, "model_weights.pth")
-    model.eval()
+    model_state_dict = torch.load(file_path)
+    model.load_state_dict(model_state_dict)
 
-    # get test dataset and loader
-    transform = Compose([ToTensor(), Normalize((0.1307,), (0.3081,))])
-    test_data = train_tutorial.load_data("data", False, True, transform, 1000)
+    return model, device
 
-    # process first 10 examples
-    data_iter = iter(test_data)
-    images, labels = next(data_iter)
 
+# this function gets the tensor output values based on the network model
+def get_predictions(num_predictions, images, labels, model, device):
+    # store all tensor outputs
+    list_outputs = []
+
+    # get first 10 examples
     with torch.no_grad():
         for i in range(10):
             # add extra dimension for PyTorch processing
             image = images[i].unsqueeze(0).to(device)
             # get output tensor based on model and image input
             output = model(image)
+            # store output in list of outputs
+            list_outputs.append(output)
             # get label with highest value in output tensor
             predicted = torch.argmax(output, dim=1).item()
             # loop through each value in output to print the category label and weight
-            for j, value in enumerate(output):
+            for j, value in enumerate(output.squeeze()):
                 print("Label {}: {:.2f}".format(j, value.item()))
             # print predicted and actual labels
             print("Predicted label: {}".format(predicted))
             print("Actual label: {}".format(labels[i]))
+
+    return list_outputs
+
+
+# this function plots the predictions in a figure
+def plot_predictions(list_output, images, rows, cols):
+    # figure to draw images on
+    figure = plt.figure(figsize=(rows * cols, rows * cols), num="Sample Images")
+    for i in range(1, rows * cols + 1):
+        img = images[i - 1].squeeze()
+        predicted = torch.argmax(list_output[i - 1]).item()
+        figure.add_subplot(3, 3, i)
+
+        plt.title(f"Prediction: {predicted}")
+        plt.axis("off")
+        plt.imshow(img, cmap="gray")
+    plt.show()
+
+
+# this function reads example images that are not in the test or trained dataset and outputs predictions using the model
+def prediction_model(images_directory, model, device):
+    # loop through each image in directory
+    files = os.listdir(images_directory)
+    for file in files:
+        if file.endswith("jpg") or file.endswith("jpeg") or file.endswith("png"):
+            print("Reading image file named: ", file)
+            # read each image
+            image = cv2.imread(os.path.join(images_directory, file))
+
+            # convert to gray image
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+            # re-size to 28x28
+            image = cv2.resize(image, (28, 28))
+
+            # switch black and white colors
+            _, image = cv2.threshold(image, 127, 255, cv2.THRESH_BINARY_INV)
+
+            # normalize intensities
+            image = image / 255.0
+
+            # get tensor image
+            tensor_image = (
+                torch.from_numpy(image).unsqueeze(0).unsqueeze(0).float().to(device)
+            )
+
+            # run each image through model
+            output = model(tensor_image)
+
+            # get prediction
+            predicted = torch.argmax(output, dim=1).item()
+
+            # re-size image for output
+            image = cv2.resize(image, (500, 500))
+
+            # write prediction on image
+            cv2.putText(
+                image,
+                "PREDICTED: " + str(predicted),
+                (25, 25),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                1,
+                (255, 255, 255),
+                3,
+                cv2.LINE_AA,
+            )
+
+            # show image
+            cv2.imshow("Prediction", image)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows
+
+
+# main function
+def main(argv):
+    # load network file
+    model, device = load_network_file("model_weights.pth")
+    # evaluation mode; not training mode
+    model.eval()
+
+    # get test dataset and loader
+    transform = Compose([ToTensor(), Normalize((0.1307,), (0.3081,))])
+    test_data = train_tutorial.load_data("data", False, True, transform, 1000)
+
+    # get first batch
+    data_iter = iter(test_data)
+    images, labels = next(data_iter)
+
+    # get first 10 examples predictions
+    list_outputs = get_predictions(10, images, labels, model, device)
+
+    # show first 9 examples
+    plot_predictions(list_outputs, images, 3, 3)
+
+    # show new images and predictions based on model
+    prediction_model("input_images", model, device)
 
     return
 
